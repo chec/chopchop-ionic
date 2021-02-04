bc146c1b1a6575710c1c70dfe6af1ba2e6ffd103
Craig Tweedy <ahoy@craigtweedy.co.uk>
2021-02-04 15:12:42 +0000

    Intial set up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>ChopChop</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">ChopChop</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
